ecc53954105a2c5e9a23d3872d83a3aa3683c8c1
konradc67 <179463@stud.prz.edu.pl>
2026-04-05 23:31:28 +0200

    chore: initialize project with package.json and essential dependencies

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+
+// Inicjalizacja aplikacji Express
+const app = express();
+
+// Middleware (odpala się przy każdym zapytaniu)
+app.use(cors()); // Zezwala na strzały z frontendu
+app.use(express.json()); // Pozwala odczytywać dane wysyłane jako JSON (np. z formularza)
+
+// Prosty endpoint (ścieżka) testowy
+app.get('/', (req, res) => {
+  res.send('Backend systemu biblioteki działa!');
+});
+
+// Tutaj będziemy dodawać kolejne funkcje, np.:
+app.get('/api/ksiazki', (req, res) => {
+  res.json([
+    { id: 1, tytul: "Wiedźmin", autor: "Andrzej Sapkowski" },
+    { id: 2, tytul: "Diuna", autor: "Frank Herbert" }
+  ]);
+});
+
+// Uruchomienie serwera
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Serwer wystartował na porcie http://localhost:${PORT}`);
+});
